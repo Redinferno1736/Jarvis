827583c2062790d57a97817c6c07717b446c5979
Redinferno1736 <code.pranavdp@gmail.com>
2025-03-21 00:31:36 +0530

    Added requirements

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from werkzeug.security import check_password_hash, generate_password_hash
 from pymongo.errors import DuplicateKeyError
-from datetime import datetime, timezone
-import io
+from datetime import datetime
 import helpers
 from slugify import slugify
 
